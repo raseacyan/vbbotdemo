d9118aa3f7604b5a2ef7714275c7ecff5889b96e
caesarnay <caesar@test.com>
2020-02-19 09:19:02 +0630

    test

--- app.js
+++ app.js
@@ -53,7 +53,14 @@ const SAMPLE_RICH_MEDIA = {
          
          {
             "Columns":6,
-            "Rows":7,
+            "Rows":3,
+            "ActionType":"open-url",
+            "ActionBody":"https://www.google.com",
+            "Image":"http://html-test:8080/myweb/guy/assets/imageRMsmall2.png"
+         },
+         {
+            "Columns":6,
+            "Rows":2,
             "Text":"<font color=#323232><b>Headphones with Microphone, On-ear Wired earphones</b></font><font color=#777777><br>Sound Intone </font><font color=#6fc133>$17.99</font>",
             "ActionType":"open-url",
             "ActionBody":"https://www.google.com",
@@ -61,6 +68,65 @@ const SAMPLE_RICH_MEDIA = {
             "TextVAlign":"middle",
             "TextHAlign":"left"
          },
+         {
+            "Columns":6,
+            "Rows":1,
+            "ActionType":"reply",
+            "ActionBody":"https://www.google.com",
+            "Text":"<font color=#ffffff>Buy</font>",
+            "TextSize":"large",
+            "TextVAlign":"middle",
+            "TextHAlign":"middle",
+            "Image":"https://s14.postimg.org/4mmt4rw1t/Button.png"
+         },
+         {
+            "Columns":6,
+            "Rows":1,
+            "ActionType":"reply",
+            "ActionBody":"https://www.google.com",
+            "Text":"<font color=#8367db>MORE DETAILS</font>",
+            "TextSize":"small",
+            "TextVAlign":"middle",
+            "TextHAlign":"middle"
+         },
+         {
+            "Columns":6,
+            "Rows":3,
+            "ActionType":"open-url",
+            "ActionBody":"https://www.google.com",
+            "Image":"https://s16.postimg.org/wi8jx20wl/image_RMsmall2.png"
+         },
+         {
+            "Columns":6,
+            "Rows":2,
+            "Text":"<font color=#323232><b>Hanes Men's Humor Graphic T-Shirt</b></font><font color=#777777><br>Hanes</font><font color=#6fc133>$10.99</font>",
+            "ActionType":"open-url",
+            "ActionBody":"https://www.google.com",
+            "TextSize":"medium",
+            "TextVAlign":"middle",
+            "TextHAlign":"left"
+         },
+         {
+            "Columns":6,
+            "Rows":1,
+            "ActionType":"reply",
+            "ActionBody":"https://www.google.com",
+            "Text":"<font color=#ffffff>Buy</font>",
+            "TextSize":"large",
+            "TextVAlign":"middle",
+            "TextHAlign":"middle",
+            "Image":"https://s14.postimg.org/4mmt4rw1t/Button.png"
+         },
+         {
+            "Columns":6,
+            "Rows":1,
+            "ActionType":"reply",
+            "ActionBody":"https://www.google.com",
+            "Text":"<font color=#8367db>MORE DETAILS</font>",
+            "TextSize":"small",
+            "TextVAlign":"middle",
+            "TextHAlign":"middle"
+         }
          
       ]
 };
